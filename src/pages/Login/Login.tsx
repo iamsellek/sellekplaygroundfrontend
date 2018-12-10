a3f685d833b31e9b99bcfe143594748192319ffb
Chris Sellek <chris.sellek@willowtreeapps.com>
2018-12-10 10:59:47 -0500

    Add state to Login component.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,23 +1,85 @@
-import {TextField} from '@material-ui/core';
+import {Button, TextField} from '@material-ui/core';
 import * as React from 'react';
 import {LoginDiv} from './components';
 
 interface State {
   email: string;
+  emailError: boolean;
   password: string;
+  passwordError: boolean;
 }
 
-class Login extends React.Component<{}, {}> {
+class Login extends React.Component<{}, State> {
+  state = {email: '', emailError: false, password: '', passwordError: false};
+
+  handleEmailChange(email: string) {
+    this.setState({email});
+  }
+
+  verifyEmail() {
+    if (
+      this.state.email.indexOf('@') === -1 ||
+      this.state.email.lastIndexOf('.') < this.state.email.indexOf('@')
+    ) {
+      this.setState({emailError: true});
+    }
+  }
+
+  handlePasswordChange(password: string) {
+    this.setState({password});
+  }
+
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div style={{width: '20%'}}>
-        <form>
+        <form
+          onSubmit={e => {
+            this.handleSubmit(e);
+          }}
+        >
           <LoginDiv>
-            <TextField fullWidth label="Email" />
+            <TextField
+              fullWidth
+              error={this.state.emailError}
+              id="email"
+              label="Email"
+              helperText={
+                this.state.emailError && 'Please enter a valid email address.'
+              }
+              onChange={e => this.handleEmailChange(e.target.value)}
+              onBlur={() => {
+                this.verifyEmail();
+              }}
+              onFocus={() => {
+                this.setState({emailError: false});
+              }}
+            />
           </LoginDiv>
           <LoginDiv>
-            <TextField fullWidth type="password" label="Password" />
+            <TextField
+              fullWidth
+              error={this.state.passwordError}
+              id="password"
+              label="Password"
+              onChange={e => this.handlePasswordChange(e.target.value)}
+              onFocus={() => {
+                this.setState({passwordError: false});
+              }}
+              type="password"
+            />
           </LoginDiv>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{marginTop: '3em', width: '100%'}}
+          >
+            Login
+          </Button>
         </form>
       </div>
     );
