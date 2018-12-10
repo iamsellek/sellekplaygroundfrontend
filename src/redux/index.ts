067c72bb3a15261aac9a87848b3ab24f645ce815
Chris Sellek <chris.sellek@willowtreeapps.com>
2018-12-10 15:40:33 -0500

    Only load redux dev tools in dev mode.

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,12 +2,17 @@ import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
+const devTools =
+  process.env.NODE_ENV === 'development'
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    : null;
+
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
